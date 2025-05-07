ff83cb19f6e4c47c745685f36f12abcfe5f7a123
Mohammad Hosseinali <mohammad.hoseinali1@gmail.com>
2025-05-07 19:07:10 +0330

    get_watch_playist method

--- ytmusicapi/mixins/watch.py
+++ ytmusicapi/mixins/watch.py
@@ -7,102 +7,16 @@ from ytmusicapi.type_alias import JsonList, ParseFuncType, RequestFuncType
 
 
 class WatchMixin(MixinProtocol):
-    def get_watch_playlist(
+    async def get_watch_playlist(
         self,
+        session,
+        proxy = None,
         videoId: str | None = None,
         playlistId: str | None = None,
         limit: int = 25,
         radio: bool = False,
         shuffle: bool = False,
     ) -> dict[str, JsonList | str | None]:
-        """
-        Get a watch list of tracks. This watch playlist appears when you press
-        play on a track in YouTube Music.
-
-        Please note that the ``INDIFFERENT`` likeStatus of tracks returned by this
-        endpoint may be either ``INDIFFERENT`` or ``DISLIKE``, due to ambiguous data
-        returned by YouTube Music.
-
-        :param videoId: videoId of the played video
-        :param playlistId: playlistId of the played playlist or album
-        :param limit: minimum number of watch playlist items to return
-        :param radio: get a radio playlist (changes each time)
-        :param shuffle: shuffle the input playlist. only works when the playlistId parameter
-            is set at the same time. does not work if radio=True
-        :return: List of watch playlist items. The counterpart key is optional and only
-            appears if a song has a corresponding video counterpart (UI song/video
-            switcher).
-
-        Example::
-
-            {
-                "tracks": [
-                    {
-                      "videoId": "9mWr4c_ig54",
-                      "title": "Foolish Of Me (feat. Jonathan Mendelsohn)",
-                      "length": "3:07",
-                      "thumbnail": [
-                        {
-                          "url": "https://lh3.googleusercontent.com/ulK2YaLtOW0PzcN7ufltG6e4ae3WZ9Bvg8CCwhe6LOccu1lCKxJy2r5AsYrsHeMBSLrGJCNpJqXgwczk=w60-h60-l90-rj",
-                          "width": 60,
-                          "height": 60
-                        }...
-                      ],
-                      "feedbackTokens": {
-                        "add": "AB9zfpIGg9XN4u2iJ...",
-                        "remove": "AB9zfpJdzWLcdZtC..."
-                      },
-                      "likeStatus": "INDIFFERENT",
-                      "videoType": "MUSIC_VIDEO_TYPE_ATV",
-                      "artists": [
-                        {
-                          "name": "Seven Lions",
-                          "id": "UCYd2yzYRx7b9FYnBSlbnknA"
-                        },
-                        {
-                          "name": "Jason Ross",
-                          "id": "UCVCD9Iwnqn2ipN9JIF6B-nA"
-                        },
-                        {
-                          "name": "Crystal Skies",
-                          "id": "UCTJZESxeZ0J_M7JXyFUVmvA"
-                        }
-                      ],
-                      "album": {
-                        "name": "Foolish Of Me",
-                        "id": "MPREb_C8aRK1qmsDJ"
-                      },
-                      "year": "2020",
-                      "counterpart": {
-                        "videoId": "E0S4W34zFMA",
-                        "title": "Foolish Of Me [ABGT404] (feat. Jonathan Mendelsohn)",
-                        "length": "3:07",
-                        "thumbnail": [...],
-                        "feedbackTokens": null,
-                        "likeStatus": "LIKE",
-                        "artists": [
-                          {
-                            "name": "Jason Ross",
-                            "id": null
-                          },
-                          {
-                            "name": "Seven Lions",
-                            "id": null
-                          },
-                          {
-                            "name": "Crystal Skies",
-                            "id": null
-                          }
-                        ],
-                        "views": "6.6K"
-                      }
-                    },...
-                ],
-                "playlistId": "RDAMVM4y33h81phKU",
-                "lyrics": "MPLYt_HNNclO0Ddoc-17"
-            }
-
-        """
         body = {
             "enablePersistentPlaylistPanel": True,
             "isAudioOnly": True,
@@ -132,7 +46,7 @@ class WatchMixin(MixinProtocol):
         if radio:
             body["params"] = "wAEB"
         endpoint = "next"
-        response = self._send_request(endpoint, body)
+        response = await self._send_request_async(session, proxy, endpoint, body)
         watchNextRenderer = nav(
             response,
             [
