bc87b926af0625a3beb0b4e24b4754f3ad894c82
Mohammad Hosseinali <mohammad.hoseinali1@gmail.com>
2025-05-22 23:47:58 +0330

    Improvement

--- ytmusicapi/ytmusic.py
+++ ytmusicapi/ytmusic.py
@@ -256,7 +256,8 @@ class YTMusicBase:
             cookies=self.cookies,
         ) as response:
             response.raise_for_status()
-            response_text: JsonDict = orjson.loads(await response.read())
+            resp = await response.read()
+            response_text: JsonDict = orjson.loads(resp)
 
             # response_text: JsonDict = await asyncio.get_event_loop().run_in_executor(None, self.decode_and_parse, await response.read())
         return response_text
